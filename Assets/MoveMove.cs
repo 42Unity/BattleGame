969dd6e3bb7b268fa7bdcac5f1ab7fc7615302ed
geonwule <geonwule@student.42seoul.kr>
2024-07-02 16:05:45 +0900

    add test script

---
+++ Assets/MoveMove.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMove : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + Time.deltaTime, 0, 0);
+    }
+}
